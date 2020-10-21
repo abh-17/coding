1bce688c01292b9d763f1091957108e23673b286
AdityaGattu <52188412+AdityaGattu@users.noreply.github.com>
2020-10-21 11:01:16 +0530

    Update missing positive.cpp

--- missing positive.cpp
+++ missing positive.cpp
@@ -1,3 +1,10 @@
+
+// every index should be matched with value ....here since started with 0 ... index should be matched with value index+1
+//negitive no no can be ignored
+//basically index 1shou;d have 1 ,2 should have to 2 ....so on....
+//in code since started from  index0 ->should have 1 ...and so on.. 
+   
+
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
