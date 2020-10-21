b08855de67d060581ef1376c508581680246a390
abh-17 <67569016+abh-17@users.noreply.github.com>
2020-10-21 10:52:28 +0530

    Create fire routes.cpp

---
+++ fire routes.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+#define MOD 1000000007
+
+int n,m,a,b;
+vector<vector<int>>v;
+vector<bool>vis;
+
+int dfs(int node,int c)
+{
+    vis[node]=true;
+    
+    for(int i=0;i<v[node].size();i++)
+    {
+        if(!vis[v[node][i]])
+        {
+            c=dfs(v[node][i],c);
+        }
+    }
+    return c+1;
+}
+
+int main() {
+	// your code goes here
+	
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+
+	    
+	    cin>>n>>m;
+	    v.assign(n,vector<int>());
+	    vis.assign(n,false);
+	    for(int i=0;i<m;i++)
+	    {
+	        cin>>a>>b;
+	        v[a-1].push_back(b-1);
+	        v[b-1].push_back(a-1);
+	        
+	    }
